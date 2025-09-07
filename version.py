c77b90cd9d49872da958fb3deb2c4a67c19ea9d0
Jikhan Jung <honestjung@gmail.com>
2025-09-07 10:22:40 +0900

    chore: bump version to 0.1.4-beta.1

--- version.py
+++ version.py
@@ -2,5 +2,5 @@
 Modan2 Version Information
 Single Source of Truth for version management
 """
-__version__ = "0.1.4"
+__version__ = "0.1.4-beta.1"
 __version_info__ = tuple(map(int, __version__.split('.')))

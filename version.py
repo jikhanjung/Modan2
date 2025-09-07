886b4a985b783f2d120a00a7d9d67ddb4d50a966
Jikhan Jung <honestjung@gmail.com>
2025-09-07 10:28:29 +0900

    fix: Make version_info compatible with pre-release versions

--- version.py
+++ version.py
@@ -2,5 +2,10 @@
 Modan2 Version Information
 Single Source of Truth for version management
 """
+import semver
+
 __version__ = "0.1.4-beta.1"
-__version_info__ = tuple(map(int, __version__.split('.')))
+
+# semver 라이브러리를 사용해 안전하게 파싱
+_ver = semver.VersionInfo.parse(__version__)
+__version_info__ = (_ver.major, _ver.minor, _ver.patch)
